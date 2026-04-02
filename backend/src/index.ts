7db34bf0642d8ababbfcd73d58036ef643efa3cf
kirti jha <kjha0141@gmail.com>
2026-04-02 13:12:25 +0530

    Allow ebazars.in in CORS origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -47,6 +47,10 @@ app.use(cors({
     process.env.FRONTEND_URL || "http://localhost:5173",
     "http://localhost:5173",
     "http://localhost:3000",
+    "https://ebazars.in",
+    "http://ebazars.in",
+    "https://kailasha247.com",
+    "http://kailasha247.com"
   ],
   credentials: true,
 }));
